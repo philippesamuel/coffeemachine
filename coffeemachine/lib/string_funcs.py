ca5e6b794d46e9b57d16c5819c7a013e7f3d2b49
Philippe Costa <philippe.costa@basf.com>
2023-11-30 10:00:36 +0100

    add Menu, CoffeMaker and MoneyMachine classes

--- coffeemachine/lib/string_funcs.py
+++
@@ -1,6 +0,0 @@
-"""Functions for string formatting."""
-
-
-def format_dollars(cents: int) -> str:
-    """Format cents as dollars."""
-    return f"${cents / 100:,.2f}"
--- coffeemachine/main.py
+++ coffeemachine/main.py
@@ -4,144 +4,42 @@ Money handled in this code is internally represented in cents of dollar.
 I use ints to handle mathematical operations on money and thus avoid inaccuracies due to floating point arithmetics.
 
 """
-from functools import partial
-
 import click
 
-from coffeemachine.config import CoffeMachineFunction
-from coffeemachine.config import COFFE_MACHINE_OPTIONS
-from coffeemachine.config import Drink
-from coffeemachine.config import DrinkSpecsDict
-from coffeemachine.config import MENU
-from coffeemachine.config import RESOURCES
-from coffeemachine.exceptions import NotEnoughMoneyError
-from coffeemachine.lib.string_funcs import format_dollars
+from coffeemachine.menu import Menu
+from coffeemachine.coffee_maker import CoffeeMaker
+from coffeemachine.money_machine import MoneyMachine
 
 
 @click.command()
 def main() -> None:
+    coffe_maker = CoffeeMaker()
+    money_machine = MoneyMachine()
+    menu = Menu()
+    coffe_machine_options = [item.name for item in menu.menu] + ['report', 'off']
     machine_is_on = True
     while machine_is_on:
-        order: str = click.prompt(f"What would you like? ({Drink.formatted_options()})",
-                                  type=click.Choice(COFFE_MACHINE_OPTIONS, case_sensitive=False),
+        order: str = click.prompt(f"What would you like? ({menu.get_items()})",
+                                  type=click.Choice(coffe_machine_options, case_sensitive=False),
                                   value_proc=str.lower,
                                   show_choices=False
                                   )
-        coffe_machine_func = options_func_mapping(option=order)
-        # TODO: instead of using a try/except block, try railway oriented programming
-        #   See ArjanCodes videos on that topic
-        try:
-            coffe_machine_func()
-        except click.Abort:
+        if order == 'off':
+            click.echo("Turning machine off!")
+            click.echo("Bye bye 👋🏾!")
             machine_is_on = False
-        except NotEnoughMoneyError:
-            continue
-
-
-def turn_off():
-    click.echo("Turning machine off!")
-    click.echo("Bye bye 👋🏾!")
-    raise click.Abort
-
-
-def print_report():
-    click.echo(
-        f"Water: {RESOURCES['water']}ml \n"
-        f"Milk: {RESOURCES['milk']}ml \n"
-        f"Coffee: {RESOURCES['coffee']}g \n"
-        f"Money: {format_dollars(RESOURCES['money'])} \n"
-    )
-
-
-def sell_drink(drink: Drink) -> None:
-    click.echo(f"Selling drink: {drink}")
-    drink_specs = MENU.get(drink)
-
-    if not drink_specs:
-        click.echo("Sorry that drink is not available 😣.")
-        return
-
-    ingredients = drink_specs["ingredients"]
-    price_cents = drink_specs["cost"]
-
-    if not check_sufficient_resources(ingredients):
-        return
-
-    handle_payment(price_cents)
-
-    for ingredient, amount_needed in ingredients.items():
-        RESOURCES[ingredient] -= amount_needed
-
-    click.echo(f"Here is your {drink} ☕. Enjoy!")
-
-
-def handle_payment(price_cents: int) -> None:
-    """Handle payment for a drink.
-
-    Arguments:
-        price_cents: Price of the drink in cents.
-
-    Raises:
-        NotEnoughMoneyError: If inserted money is not enough to buy a drink.
-    """
-    inserted_money_cents = request_money()
-
-    # check if inserted money is enough to buy a drink.
-    if inserted_money_cents < price_cents:
-        click.echo("Sorry that's not enough money. Money refunded. 🪙🪙🪙")
-        raise NotEnoughMoneyError
-
-    # handle change
-    change = inserted_money_cents - price_cents
-    if change:
-        click.echo(f"Here is {format_dollars(change)} dollars in change. 🪙🪙🪙")
-
-    # update machine money
-    RESOURCES["money"] += price_cents
-
-
-def request_money() -> int:
-    click.echo("Please insert coins 🪙🪙🪙")
-    quarters = click.prompt("How many quarters?", type=int)
-    dimes = click.prompt("How many dimes?", type=int)
-    nickles = click.prompt("How many nickles?", type=int)
-    pennies = click.prompt("How many pennies?", type=int)
-    inserted_money_cents = (
-            quarters * 25
-            + dimes * 10
-            + nickles * 5
-            + pennies
-    )
-    click.echo(f"You inserted {format_dollars(inserted_money_cents)}")
-    return inserted_money_cents
-
-
-def check_sufficient_resources(ingredients: DrinkSpecsDict) -> bool:
-    insufficient_resources = get_insufficient_resources(ingredients)
-    if insufficient_resources:
-        for resource in insufficient_resources:
-            click.echo(f"Sorry there is not enough {resource} 😣.")
-    return False if insufficient_resources else True
-
-
-def get_insufficient_resources(ingredients: DrinkSpecsDict) -> list[str]:
-    return [ingredient for ingredient, amount_needed in ingredients.items() if amount_needed > RESOURCES[ingredient]]
-
-
-def invalid_option() -> None: click.echo("Option invalid")
-
-
-OPTIONS_FUNC_DICT: dict[str, CoffeMachineFunction] = {
-    'off': turn_off,
-    'report': print_report,
-    Drink.ESPRESSO: partial(sell_drink, drink=Drink.ESPRESSO),
-    Drink.LATTE: partial(sell_drink, drink=Drink.LATTE),
-    Drink.CAPPUCCINO: partial(sell_drink, drink=Drink.CAPPUCCINO)
-}
-
-
-def options_func_mapping(option: str) -> CoffeMachineFunction:
-    return OPTIONS_FUNC_DICT.get(option, invalid_option)
+        elif order == 'report':
+            coffe_maker.report()
+            money_machine.report()
+        else:
+            drink = menu.find_drink(order)
+            if not drink:
+                continue
+            if not coffe_maker.is_resource_sufficient(drink):
+                continue
+            if not money_machine.make_payment(drink.cost):
+                continue
+            coffe_maker.make_coffee(drink)
 
 
 if __name__ == '__main__':
